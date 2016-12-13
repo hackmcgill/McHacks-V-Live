a8a9e60e0bb54120d174e9aaa125236a44e782b7
chriswongtv <chris@cogifire.com>
2016-12-13 21:07:22 +0800

    Streamlined tab navigation

--- js/script.js
+++ js/script.js
@@ -21,21 +21,13 @@ $(function() {
 	$(".menu>a").click(function() {
 		$(".active").removeClass("active");
 		$(this).addClass("active");
-	});
 
-	$("#announcements-btn").click(function() {
+		var tab = $(this).attr('href');
+
 		$(".active-tab").fadeToggle(400, "swing", function() {
 			$(".active-tab").removeClass("active-tab");
-			$("#announcements").fadeToggle();
-			$("#announcements").addClass("active-tab");
-		});
-	});
-
-	$("#mentor-btn").click(function() {
-		$(".active-tab").fadeToggle(400, "swing", function() {
-			$(".active-tab").removeClass("active-tab");
-			$("#mentor").addClass("active-tab");
-			$("#mentor").fadeToggle();
-		});
+			$(tab).fadeToggle();
+			$(tab).addClass("active-tab");
+		})
 	});
 });
