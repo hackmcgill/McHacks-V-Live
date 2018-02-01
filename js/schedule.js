dc2ed80737c47677cc464630b714afe224048c9e
Ryan Burgett <thesonicdoctor@gmail.com>
2018-02-01 09:23:38 -0500

    added location to schedule events

--- js/schedule.js
+++ js/schedule.js
@@ -1,6 +1,6 @@
 var schedule = [
     {
-        "title": "Out of Town Check-In",
+        "title": "Out of Town Check-In in SSMU 108",
         "description": "If you are from out of Montreal this is when you check-in",
         "day": "Sat",
         "starts": "9:00 AM",
@@ -9,7 +9,7 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "From Montreal Check-In",
+        "title": "From Montreal Check-In in SSMU 108",
         "day": "Sat",
         "description": "If you are from Montreal this is when you check-in",
         "starts": "9:30 AM",
@@ -18,7 +18,7 @@ var schedule = [
         "orientation": "left"
     },
     {
-        "title": "Opening Ceremonies   ",
+        "title": "Opening Ceremonies in McMed",
         "description": "Opening Ceremonies, held at McMed",
         "day": "Sat",
         "starts": "10:30 AM",
@@ -32,7 +32,7 @@ var schedule = [
         "duration": 15
     },
     {
-        "title": "Team Formation",
+        "title": "Team Formation in Lev Bukman",
         "description": "Help find teammates in Lev Bukhman",
         "day": "Sat",
         "starts": "12:15 PM",
@@ -40,7 +40,7 @@ var schedule = [
         "orientation": "left"
     },
     {
-        "title": "Lunch",
+        "title": "Lunch in Sadies",
         "description": "Come have lunch at", 
         "day": "Sat",
         "starts": "12:30 PM",
@@ -48,7 +48,7 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "Lighthouse Labs Workshop",
+        "title": "Lighthouse Labs Workshop in Madeline Parent",
         "description": "Workshop held by Lighthouse Labs in Madeline Parent",
         "day": "Sat",
         "starts": "1:00 PM",
@@ -56,7 +56,7 @@ var schedule = [
         "orientation": "left"
     },
     {
-        "title": "Google Workshop",
+        "title": "Google Workshop in Lev Bukhman",
         "description": "Workshop held by Google in Lev Bukhman",
         "day": "Sat",
         "starts": "1:00 PM",
@@ -64,7 +64,7 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "Nuance Workshop",
+        "title": "Nuance Workshop in Lev Bukhman",
         "description": "Workshop held by Nuance in Lev Bukhman",
         "day": "Sat",
         "starts": "2:00 PM",
@@ -72,7 +72,7 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "Thinking Capital Workshop",
+        "title": "Thinking Capital Workshop in Madeline Parent",
         "description": "Workshop held by Thinking Capital in Madeline Parent",
         "day": "Sat",
         "starts": "3:00 PM",
@@ -80,7 +80,7 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "Challenge: Loyalty 1",
+        "title": "Challenge: Loyalty 1 in Lev Bukhman",
         "description": "A Challenge held in Lev Bukhman by Loyalty 1",
         "day": "Sat",
         "starts": "3:00 PM",
@@ -96,7 +96,7 @@ var schedule = [
         "orientation": "left"
     },
     {
-        "title": "Happy Hour at Gerts",
+        "title": "Happy Hour in Gerts",
         "description": "The Happiest Hours at McHacks",
         "day": "Sat",
         "starts": "5:00 PM",
@@ -104,14 +104,14 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "Dinner",
+        "title": "Dinner in Sadies",
         "description": "Come have dinner at",
         "day": "Sat",
         "starts": "7:00 PM",
         "duration": 60
     },
     {
-        "title": "PhotoBooth at Gerts",
+        "title": "PhotoBooth in Gerts",
         "description": "Take Photos of your fun time at McHacks at Gerts in the basement",
         "day": "Sat",
         "starts": "8:00 PM",
@@ -119,7 +119,7 @@ var schedule = [
         "orientation": "left"
     },
     {
-        "title": "Women in Tech Meetup",
+        "title": "Women in Tech Meetup in SSMU 108",
         "description": "A Meetup for Women/Minorites in Room 108",
         "day": "Sat",
         "starts": "8:30 PM",
@@ -127,7 +127,7 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "High School Hackers Meetup",
+        "title": "High School Hackers Meetup SSMU 108",
         "description": "A Meetup for High School Hackers in Room 108",
         "day": "Sat",
         "starts": "9:30 PM",
@@ -135,27 +135,27 @@ var schedule = [
         "orientation": "right"
     },
     {
-        "title": "Midnight Snack ",
+        "title": "Midnight Snack in Sadies",
         "day": "Sun",
         "starts": "12:00 AM",
         "duration": 30
     },
     {
-        "title": "Breakfast",
+        "title": "Breakfast in Sadies",
         "day": "Sun",
         "description": "Breakfast in Sadies",
         "starts": "8:00 AM",
         "duration": 60
     },
     {
-        "title": "Devpost Registration Deadline",
+        "title": "Devpost Registration Deadline at the Kiosk",
         "day": "Sun",
         "description": "Must go to the Kiosk and register your hack",
         "starts": "10:30 AM",
         "duration": 30
     },
     {
-        "title": "Lunch",
+        "title": "Lunch in Sadies",
         "day": "Sun",
         "starts": "11:30 AM",
         "duration": 60,
@@ -170,14 +170,14 @@ var schedule = [
         "orientation": "left"
     },
     {
-        "title": "Project Expo",
+        "title": "Project Expo in the Caf and Ballroom",
         "day": "Sun",
         "starts": "12:30 PM",
         "duration": 90,
         "orientation": "left"
     },
     {
-        "title": "Closing Ceremony",
+        "title": "Closing Ceremony in McMed",
         "day": "Sun",
         "starts": "3:00 PM",
         "duration": 60
